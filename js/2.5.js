147325a06cf898b26125514490166c426651d42e
Anton1273751 <kronoss161@gmail.com>
2023-10-12 15:33:31 +0300

    js-start

---
+++ js/2.5.js
@@ -0,0 +1,14 @@
+//Типы данных
+
+// let n = 123123123;
+// n = 12125125.345;
+
+// let str = "Привет";
+// let str2 = "Одинарные кавычки тоже подойдут";
+// let phrase = `Обратные кавычки позволяют встраивать переменные ${str}`;
+
+// let name = "Ilya";
+
+// alert(`hello ${1}`) // hello 1
+// alert(`hello ${"name"}`)  // hello name
+// alert( `hello ${name}` ) // hello Ilya
---
+++ js/2.6.js
@@ -0,0 +1,15 @@
+// Взаимодействие: alert, prompt, confirm
+
+// let age = prompt("Сколько тебе лет?", 50);
+// alert(`Тебе ${age} лет`);
+
+//Функция confirm отображает модальное окно с текстом вопроса question и двумя кнопками: OK и Отмена.
+
+// let question = confirm("Учишь Js?");
+// alert(question);
+
+// Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+
+let userName = prompt("Как вас зовут?");
+let userAge = prompt("Сколько вам лет?");
+alert(`Вас зовут ${userName}, вам ${userAge} лет.`);
