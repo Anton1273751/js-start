ce98a48f021af1a55d6fd5e676b242b370940b1b
Anton1273751 <kronoss161@gmail.com>
2023-10-25 15:58:28 +0300

    js-start

--- js/5.1.js
+++ js/5.1.js
@@ -1,1 +1,17 @@
 //Методы примитивов
+
+// let str = "hellwero";
+// str.toUpperCase()
+
+// console.log(str.toLocaleUpperCase());
+
+
+// let num = 124.2452;
+
+// console.log(num.toFixed(1))
+
+// let str = "Привет";
+
+// str.test = 5;
+
+// alert(str.test);
