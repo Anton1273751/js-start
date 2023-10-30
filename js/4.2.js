044321b79a052ded120d8ecd602c2f2cf71cca09
Anton1273751 <kronoss161@gmail.com>
2023-10-30 13:57:14 +0300

    js-start

--- js/4.2.js
+++ js/4.2.js
@@ -20,3 +20,20 @@
 // clone.name = "Pete"; // изменим в нём данные
 
 // alert( user.name ); // все ещё John в первоначальном объекте
+
+// var obj = { a: 1 };
+// var copy = Object.assign({}, obj);
+// console.log(copy); // { a: 1 }
+
+
+// Пример: слияние объектов
+
+// js
+
+// var o1 = { a: 1 };
+// var o2 = { b: 2 };
+// var o3 = { c: 3 };
+
+// var obj = Object.assign(o1, o2, o3);
+// console.log(obj); // { a: 1, b: 2, c: 3 }
+// console.log(o1); // { a: 1, b: 2, c: 3 }, изменился и сам целевой объект.
---
+++ js/4.4.js
@@ -0,0 +1,32 @@
+// Методы объекта, "this"
+
+// let user = {
+//   userName: "Anton",
+//   age: 33,
+//   sayHi: function () {
+//     alert("Hi");
+//   },
+// };
+
+// console.log(user.sayHi());
+
+
+// // сокращённая запись
+// user = {
+//   sayHi() { // то же самое, что и "sayHi: function(){...}"
+//     alert("Привет");
+//   }
+// };
+
+// let user = {
+//   name: "John",
+//   age: 30,
+
+//   sayHi() {
+//     // "this" - это "текущий объект".
+//     alert(this.name);
+//   }
+
+// };
+
+// user.sayHi(); // John
