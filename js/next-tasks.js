e7cee259dd9c591a7a890fdefe8d14b2bdd94c64
Anton1273751 <kronoss161@gmail.com>
2023-10-30 11:16:09 +0300

    js-start

--- js/next-tasks.js
+++ js/next-tasks.js
@@ -34,3 +34,47 @@
 
 // let num = Math.sqrt(379);
 
+// console.log(num.toFixed());
+// console.log(num.toFixed(1));
+// console.log(num.toFixed(2));
+
+// 5
+// Найдите квадратный корень из 587. Округлите результат в большую и меньшую стороны,
+// запишите результаты округления в объект с ключами 'floor' и 'ceil'.
+
+// let numb = 587;
+
+// let getSquareRoot = (num) => {
+//   let result = Math.sqrt(num);
+//   return result;
+// };
+
+// console.log(Math.ceil(getSquareRoot(numb)));
+// console.log(Math.floor(getSquareRoot(numb)));
+
+// 6
+// // Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число.
+
+// let numbMax = Math.max(4, -2, 5, 19, -130, 0, 10);
+// let numbMin = Math.min(4, -2, 5, 19, -130, 0, 10);
+
+// console.log(numbMax);
+// console.log(numbMin);
+
+// 7
+// Выведите на экран случайное целое число от 1 до 100.
+
+// let numbRandom = Math.random() * 100;
+// console.log(Math.round(numbRandom));
+
+// 8
+// С помощью цикла заполните массив 10-ю случайными целыми числами.
+
+// let array = [];
+
+// for (let index = 0; index < 10; index++) {
+//   array.push(Math.round(Math.random() * 100));
+// }
+
+// console.log(array);
+
