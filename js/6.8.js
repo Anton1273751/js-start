fb0cd4bc7f8c1d72c206ae6f474da73a93b2c7ce
Anton1273751 <kronoss161@gmail.com>
2023-12-05 14:42:26 +0300

    js-start

--- js/6.8.js
+++ js/6.8.js
@@ -1,1 +1,13 @@
 //Планирование: setTimeout и setInterval
+
+//setTimeout позволяет вызвать функцию один раз через определённый интервал времени.
+
+//setInterval позволяет вызывать функцию регулярно, повторяя вызов через определённый интервал времени.
+
+const printHello = (text) => {
+  console.log(text);
+};
+
+let timeID = setTimeout(printHello, 3000, "hello");
+
+clearTimeout(timeID);
